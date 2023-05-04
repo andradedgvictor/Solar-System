ded708e4c99dea1182777c9113cf16b727431418
andradedgvictor <victorandrademg@outlook.com>
2023-05-04 15:36:00 -0300

    feat: Create PlanetCard component

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <h2>{planetName}</h2>
+        <img src={ planetImage } alt="Planeta Terra" />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
